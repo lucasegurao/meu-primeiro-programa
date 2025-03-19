abb7c120cc44f7cf3fb8f0a980303032456a6cf2
Lucas Oliveira <lucasegurao@gmail.com>
2025-03-19 20:52:37 -0300

    desenvolvidos em aula

---
+++ script.js
@@ -0,0 +1,34 @@
+// var dado = "Lucas" /* String - Conjunto de Caracteres */
+// alert(typeof dado)
+
+// var dado = 123 /* Number */
+// alert(typeof dado)
+
+// var dado = true /* Boolean - true e false */
+// alert(typeof dado)
+
+// var dado /* Undefined */
+// alert(typeof dado)
+
+
+
+
+/* OPERADORES ARITMÉTICOS */
+/* 
+
+// Adição            9 + 3 = 12
+// Subtração         9 - 3 = 6
+// Divisão           9 / 3 = 3
+// Multiplicação     9 * 3 = 27
+// Potência          9 ** 3 = 729  (9 * 9 * 9)
+// Resto da Divisão  9 % 3 = 0     (0, 1 e 2)
+
+*/
+
+// var numero1 = 9
+// var numero2 = 3
+
+// var soma = numero1 + numero2
+
+// alert(soma)
+
